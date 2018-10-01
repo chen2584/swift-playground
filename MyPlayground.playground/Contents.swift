e6406d2a4b4a1f52fddfdcfeac406985039872dc
Worameth Semapat <31945018+chen2584@users.noreply.github.com>
2018-10-01 16:52:46 +0700

    add function with internal

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -39,8 +39,19 @@ func singleArg_method(arg1:String) {
 func multipleArg_method(arg1:String, arg2:Int, arg3:Bool) {
     print("arg1: \(arg1), arg2: \(arg2), arg3: \(arg3)")
 }
+
+func multipleArg_methodWithExternal(a1 arg1:String, a2 arg2:Int, arg3:Bool) {
+    print("arg1: \(arg1), arg2: \(arg2), arg3: \(arg3)")
+}
+
+func multipleArg_methodWithoutExternal(_ arg1:String, _ arg2:Int, _ arg3:Bool) {
+    print("arg1: \(arg1), arg2: \(arg2), arg3: \(arg3)")
+}
+
 simple_method()
 let result:String = return_method()
 print(result, terminator: "")
 singleArg_method(arg1: "Chen")
 multipleArg_method(arg1: "codemobile", arg2: 1234, arg3: false)
+multipleArg_methodWithExternal(a1: "1234", a2: 1234, arg3: false)
+multipleArg_methodWithoutExternal("Chen", 1, true)
