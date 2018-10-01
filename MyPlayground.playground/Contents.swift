3572f564513299ce312cdf08c7f4964aef4c6b7f
Worameth Semapat <31945018+chen2584@users.noreply.github.com>
2018-10-01 16:21:54 +0700

    add function

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,6 +2,9 @@
 
 import UIKit
 
+//*********************************
+// Variable Declaration
+//*********************************
 var str = "Hello, playground"
 var tmp1 = "CodeMobiles"
 var tmp2 = "CodeMobiles"
@@ -11,8 +14,33 @@ var tmp3 = 1
 var tmp4 = true
 
 // Explicit
-var tmp5: String = "Chen"
-var tmp6: Int = 123
-var tmp7: Bool = false
+var tmp5:String = "Chen"
+var tmp6:Int = 123
+var tmp7:Bool = false
 
 print(tmp1)
+print("Hello", terminator: "")
+
+//*********************************
+// Function Declaration
+//*********************************
+func simple_method() {
+    print("1234")
+}
+
+func return_method()->String {
+    return "CodeMobiles"
+}
+
+func singleArg_method(arg1:String) {
+    print(arg1)
+}
+
+func multipleArg_method(arg1:String, arg2:Int, arg3:Bool) {
+    print("arg1: \(arg1), arg2: \(arg2), arg3: \(arg3)")
+}
+simple_method()
+let result:String = return_method()
+print(result, terminator: "")
+singleArg_method(arg1: "Chen")
+multipleArg_method(arg1: "codemobile", arg2: 1234, arg3: false)
