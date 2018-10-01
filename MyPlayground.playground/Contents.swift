a377d558e10f5a22e9f8f020907b6179b7360647
Worameth Semapat <31945018+chen2584@users.noreply.github.com>
2018-10-01 14:00:16 +0700

    first init

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var tmp1 = "CodeMobiles"
+var tmp2 = "CodeMobiles"
+
+// Implicit
+var tmp3 = 1
+var tmp4 = true
+
+// Explicit
+var tmp5: String = "Chen"
+var tmp6: Int = 123
+var tmp7: Bool = false
+
+print(tmp1)
